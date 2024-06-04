2e50bde17b5dbd17f89ed816b9ee439a2db65348
keatonLiu <lxtxiaotong@foxmail.com>
2024-06-04 10:22:55 +0800

    fix container conflict

--- entrypoint.sh
+++ entrypoint.sh
@@ -133,10 +133,10 @@ $remote_registry_login
 log 'Launching docker compose... \"$remote_docker_exec\"';
 cd \"$remote_path\";
 $DOCKER_ENV docker compose -f \"$DOCKER_COMPOSE_FILENAME\" pull;
-$DOCKER_ENV $remote_docker_exec
-
-if [ $? -ne 0 ]; then
-  echo 'Failed to do compose up, try rm containers and try again.'
+OUTPUT=$($DOCKER_ENV $remote_docker_exec);
+echo ${OUTPUT};
+if [[ $OUTPUT == *\"Error response from daemon: Conflict.\"* ]]; then
+  echo 'Failed to do compose up, rm containers and try again.'
   $DOCKER_ENV docker compose -f \"$DOCKER_COMPOSE_FILENAME\" rm -sf
   $DOCKER_ENV ${remote_docker_exec};
 fi
